03060ad26bc9b04df54104f3475463bb654d11d8
Luiz Eugênio Filho <43796789+lesaf92@users.noreply.github.com>
2024-02-08 14:11:11 -0300

    motor functions

--- motor_driver.py
+++ motor_driver.py
@@ -7,24 +7,36 @@ in2 = 27
 # Right wheel
 in3 = 23
 in4 = 24
-# Config
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1, GPIO.OUT)
-GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(in3, GPIO.OUT)
-GPIO.setup(in4, GPIO.OUT)
+def config():
+    # Config
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(in1, GPIO.OUT)
+    GPIO.setup(in2, GPIO.OUT)
+    GPIO.setup(in3, GPIO.OUT)
+    GPIO.setup(in4, GPIO.OUT)
 
-GPIO.output(in1, GPIO.HIGH)
-GPIO.output(in2, GPIO.LOW)
-GPIO.output(in3, GPIO.HIGH)
-GPIO.output(in4, GPIO.LOW)
+def left_forward():
+    GPIO.output(in1, GPIO.HIGH)
+    GPIO.output(in2, GPIO.LOW)
+def left_backward():
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.HIGH)
+def right_forward():
+    GPIO.output(in3, GPIO.HIGH)
+    GPIO.output(in4, GPIO.LOW)
+def right_backward():
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.HIGH)
+def left_stop():
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+def right_stop():
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
 
-sleep(3)
-
-
-GPIO.output(in1, GPIO.LOW)
-GPIO.output(in2, GPIO.LOW)
-GPIO.output(in3, GPIO.LOW)
-GPIO.output(in4, GPIO.LOW)
+config()
+left_backward()
+left_forward()
+left_stop()
 
 GPIO.cleanup()
