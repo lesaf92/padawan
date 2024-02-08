a4da5f76336aed1c6b134dcaaa48a646c21df5f4
Luiz Eugênio Filho <43796789+lesaf92@users.noreply.github.com>
2024-02-08 15:27:55 -0300

    motor driver ident

--- motor_driver.py
+++ motor_driver.py
@@ -5,7 +5,6 @@ from geometry_msgs.msg import Twist
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.cleanup()
 # Left wheel
 in1 = 17
 in2 = 27
@@ -15,6 +14,7 @@ in4 = 24
 
 def config():
     # Config
+	GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(in1, GPIO.OUT)
     GPIO.setup(in2, GPIO.OUT)
@@ -60,8 +60,8 @@ def cmd_vel_cb(msg):
 ############################# Main starts here ################################
 ###############################################################################
 def main():
-    config()
-	rospy.init_node('motor_driver', anonymous=False)
+	config()
+    rospy.init_node('motor_driver', anonymous=False)
     rate = rospy.Rate(10)
     rospy.Subscriber('/cmd_vel', Twist, cmd_vel_cb)
     rospy.spin()
