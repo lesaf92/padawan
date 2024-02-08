24cee980160d774c5c83e797c8f9bc53dc82870a
Luiz Eugênio Filho <43796789+lesaf92@users.noreply.github.com>
2024-02-08 11:32:12 -0300

    motor_driver first test

---
+++ motor_driver.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+# Left wheel
+in1 = 17
+in2 = 27
+# Right wheel
+in3 = 23
+in4 = 24
+# Config
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
+GPIO.output(in1, GPIO.HIGH)
+GPIO.output(in2, GPIO.LOW)
+GPIO.output(in3, GPIO.HIGH)
+GPIO.output(in4, GPIO.LOW)
