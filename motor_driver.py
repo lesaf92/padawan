612d221e1d726950d74ff233dcc2d4290ecad7e9
Luiz Eugênio Filho <43796789+lesaf92@users.noreply.github.com>
2024-02-12 11:38:54 -0300

    added comments

--- motor_driver.py
+++ motor_driver.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
+'''
+ROS node for controlling the motors
+This code provides the hardware-level control of the wheels for the PADAWAN robot.
+
+input: cmd_vel [geometry_msgs/Twist]
+output: motor drive
+
+Since the motors do not have any way of measuring rotation, we used fixed PWM based on the signal of the control input.
+
+'''
 import rospy
 from geometry_msgs.msg import Twist
 
 import RPi.GPIO as GPIO
-from time import sleep
 
+###############################################################################
+############################# Global variables ################################
+###############################################################################
 # Left wheel
 in1 = 17
 in2 = 27
@@ -16,8 +28,12 @@ in4 = 24
 en2 = 18
 pwm2 = []
 
+# Global default PWM duty cycle
 dc_global = 30
-
+###############################################################################
+############################### Aux Functions #################################
+###############################################################################
+# Initial configuration of GPIO pins
 def config():
     global in1, in2, in3, in4, en1, en2, pwm1, pwm2
     # Config
@@ -33,8 +49,7 @@ def config():
     pwm2 = GPIO.PWM(en2, 50)
     
 
-
-
+# Primitive functions to drive motors, separated in left and right
 def left_forward(dc):
     global in1, in2, in3, in4, en1, en2, pwm1, pwm2
     GPIO.output(in1, GPIO.HIGH)
@@ -65,6 +80,7 @@ def right_stop():
     GPIO.output(in3, GPIO.LOW)
     GPIO.output(in4, GPIO.LOW)
     pwm2.stop()
+# The weak parameter is boolean and should be used if we have composite motion
 def vel_left_wheel(vel, weak):
     global dc_global
     dc = dc_global
@@ -87,13 +103,15 @@ def vel_right_wheel(vel, weak):
         right_backward(dc)
     else:
         right_stop()
-# Default values for padawan geometry
+# Inverse kinematics. Used to compute wheel velocity based on body velocity commands
+# The default arguments are the values specific for padawan geometry
 def inverse_kinematics_diff_drive(vx, wz, L=0.26, r=0.047):
     v_left = (vx - (L/2)*wz)/r
     v_right = (vx + (L/2)*wz)/r
     return v_left, v_right
 
-def motion_fixed_pwm(v_left, v_right):
+# Based on the wheel velocities, apply the fixed PWM accordingly
+def apply_fixed_pwm(v_left, v_right):
     if (v_left > v_right) and (v_left*v_right > 0):
         vel_left_wheel(v_left, False)
         vel_right_wheel(v_right, True)
@@ -104,11 +122,18 @@ def motion_fixed_pwm(v_left, v_right):
         vel_left_wheel(v_left, False)
         vel_right_wheel(v_right, False)
 
+# Final function to call. It uses all the functions above to move the robot
+def move_robot(vx, wz):
+    v_left, v_right = inverse_kinematics_diff_drive(vx, wz)
+    apply_fixed_pwm(v_left, v_right)
+
+###############################################################################
+############################## ROS callbacks ##################################
+###############################################################################
 def cmd_vel_cb(msg):
     vx = msg.linear.x
     wz = msg.angular.z
-    v_left, v_right = inverse_kinematics_diff_drive(vx, wz)
-    motion_fixed_pwm(v_left, v_right)
+    move_robot(vx, wz)
 ###############################################################################
 ############################# Main starts here ################################
 ###############################################################################
@@ -120,7 +145,7 @@ def main():
     rospy.spin()
     GPIO.cleanup()
 
-######################################################################################################
+###############################################################################
 if __name__ == "__main__":
     try:
         main()
