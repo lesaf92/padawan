27630a77026566be2793e7bab69714dc2dd17dda
Luiz Eugênio Filho <43796789+lesaf92@users.noreply.github.com>
2024-02-14 19:16:45 -0300

    imu_driver in ros

--- src/imu_driver.py
+++ src/imu_driver.py
@@ -7,26 +7,34 @@ import time
 import smbus
 
 from imusensor.MPU9250 import MPU9250
-
-address = 0x68
-bus = smbus.SMBus(1)
-imu = MPU9250.MPU9250(bus, address)
-imu.begin()
-
+'''
 print('Calibrating gyro...')
 imu.caliberateGyro()
 print('Done.')
 print('Calibrating acc...')
 imu.caliberateAccelerometer()
 print('Done.')
+'''
 
 # or load your own caliberation file
 #imu.loadCalibDataFromFile("/home/pi/calib_real_bolder.json")
-
-while True:
-	imu.readSensor()
-	imu.computeOrientation()
-	print(imu.AccelVals)
-	print(imu.GyroVals)
-	print(imu.MagVals)
-	print("roll: {0} ; pitch : {1} ; yaw : {2}".format(imu.roll, imu.pitch, imu.yaw))
+###############################################################################
+############################# Main starts here ################################
+###############################################################################
+def main():
+    rospy.init_node('imu_driver', anonymous=False)
+    rate = rospy.Rate(100)
+    imu_pub = rospy.Publisher('imu_data', Float64MultiArray, queue_size=10)
+    while not rospy.is_shutdown():
+        imu.readSensor()
+        imu.computeOrientation()
+        full_info = Float64MultiArray(data=[imu.AccelVals, imu.GyroVals, imu.MagVals, imu.roll, imu.pitch, imu.yaw])
+        imu_pub.publish(full_info)
+        rate.sleep()
+        
+###############################################################################
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
